0f8c2c59dd4691ddf84876f8a4392a624b17eef8
Jugg1 <adeoluwaadeboye@gmail.com>
2020-10-07 17:27:57 +0100

    NPM Scripts, VSCode extensions take care of this

---
+++ js/dist/scripts.dev.js
@@ -0,0 +1,33 @@
+"use strict";
+
+$(document).ready(function () {
+  //for tooltip implementation
+  // $(document).ready(function () {
+  //    $('[data-toggle = "tooltip"').tooltip();
+  //});
+  //for carousel pause and plau
+  //seems the interval is not working for me
+  $("#mycarousel").carousel({
+    interval: 2000
+  });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+      $("#mycarousel").carousel("pause");
+      $("#carouselButton").children("span").removeClass("fa-pause");
+      $("#carouselButton").children("span").addClass("fa-play");
+    } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+      $("#mycarousel").carousel("cycle");
+      $("#carouselButton").children("span").removeClass("fa-play");
+      $("#carouselButton").children("span").addClass("fa-pause");
+    }
+
+    ;
+  }); //Modal Tables with jQuery
+
+  $("#reservation").click(function () {
+    $('#reservationModal').modal('show');
+  });
+  $("#loginstuff").click(function () {
+    $('#loginModal').modal('show');
+  });
+});
